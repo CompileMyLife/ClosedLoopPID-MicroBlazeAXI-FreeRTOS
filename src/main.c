a58df9540bdf440cf1ee54f3b5cb6cf220445500
CompileMyLife <ibcinematografy@gmail.com>
2024-03-03 20:00:53 -0800

    added comments and deleted unused macros and AXI peripherals

--- src/main.c
+++ src/main.c
@@ -22,7 +22,7 @@
  * for system operation.
  *
  * @author Ibrahim Binmahfood & Robert Wilcox; integration of Fusion library by x-io Technologies.
- * @date 2024-02-26
+ * @date 2024-03-03
  */
 
 #include <stdio.h>
@@ -47,38 +47,38 @@
 #include "nexys4IO.h"
 #include "pid_controller.h"
 #include "Fusion.h"
-#include "task.h"
 
 
-
-// Device IDs
+// Alias for Interrupt Controller Peripheral
 #define INTC_DEVICE_ID          XPAR_MICROBLAZE_0_AXI_INTC_DEVICE_ID
-#define IIC_DEVICE_ID           XPAR_IIC_0_DEVICE_ID
-#define UART_DEVICE_ID          XPAR_AXI_UARTLITE_0_DEVICE_ID
-#define UART_BASEADDR           XPAR_AXI_UARTLITE_0_BASEADDR
 
-// Definitions for peripheral NEXYS4IO
+// Alias for I2C Controller Peripheral
+#define IIC_DEVICE_ID           XPAR_IIC_0_DEVICE_ID
+
+// Alias for UART Lite Peripheral 
+#define UART_BASEADDR           XPAR_AXI_UARTLITE_0_BASEADDR
+#define AXI_UARTLITE_INTR_NUM   XPAR_MICROBLAZE_0_AXI_INTC_AXI_UARTLITE_0_INTERRUPT_INTR
+
+// Aliases for NEXYS4IO Peripheral
 #define N4IO_DEVICE_ID          XPAR_NEXYS4IO_0_DEVICE_ID
 #define N4IO_BASEADDR           XPAR_NEXYS4IO_0_S00_AXI_BASEADDR
 #define N4IO_HIGHADDR           XPAR_NEXYS4IO_0_S00_AXI_HIGHADDR
 
-// AXI INTERRUPTS
+// Alias for AXI Timer Peripheral
 #define AXI_TIMER_INTR_NUM      XPAR_MICROBLAZE_0_AXI_INTC_AXI_TIMER_0_INTERRUPT_INTR
-#define AXI_UARTLITE_INTR_NUM   XPAR_MICROBLAZE_0_AXI_INTC_AXI_UARTLITE_0_INTERRUPT_INTR
 
-// BTN/SW MASKS
+// Masks
 #define SWITCH_MASK             0xFFFF
 #define BUTTON_MASK             0xFF
 
-// FUSION DEFINITONS
-#define GYRO_TO_RADIANS 0.00013323124061025415
-#define ACCEL_TO_G (1.0 / 16384.0)
+#define TOTAL_LEDS              16
+#define MID_LED                 (TOTAL_LEDS / 2)
 
-// LED definitions for "level"
-#define TOTAL_LEDS 16
-#define MID_LED TOTAL_LEDS / 2
-#define MAX_ANGLE 180 // Max target angle
+// Conversion Definitions
+#define GYRO_TO_RADIANS         0.00013323124061025415
+#define ACCEL_TO_G              (1.0 / 16384.0)
 
+#define MAX_ANGLE               180 // Max target angle
 
 // TASKS
 
@@ -176,7 +176,7 @@ void updateLEDs(real_t currentAngle, real_t targetAngle);
  */
 void readAngle(char* buffer, int bufferSize);
 
-// Global Variables
+// Global Variable
 volatile uint displayMode;
 
 // data struct
@@ -202,25 +202,26 @@ TaskHandle_t xExit = NULL;
 // AXI Peripherals
 XIntc       irq_Ctrlr;
 XIic        i2c_dev;
-XUartLite   uart_dev;
 XIic_Config *ConfigPtr;
 
-// PID
+// Initialize PID Data structure
 SPid pid;
 
+// Initialize Gyro Data structure
 GyroData gyroData;
 
-// Fusion ahrs
+// Initialize Fusion ahrs
 FusionAhrs ahrs;
 
-// System state
+// Initialize System state
 SystemState systemState;
 
 int main(void) {
 
-    // Initialize platform
+    // Initialize microblaze platform
     init_platform();
 
+    // Initialize the embedded system
     if (init_sys() != XST_SUCCESS) {
         cleanup_platform();
         return -1;
@@ -229,10 +230,12 @@ int main(void) {
     // Inittialize Fusion AHRS
     FusionAhrsInitialise(&ahrs);
 
-    // Sleep to let stuff initialize 
+    // Sleep for 1 sec to let stuff initialize 
     usleep(1000000);
 
-    // Create tasks
+    // Create the FreeRTOS Tasks with Minimal Stack Sizes and same priority level
+    // The Data Task requires more stack space so it is multiplied by 3 to
+    // prevent stackoverflows due to Fusion pkg being math heavy
     xTaskCreate(vPIDTask,  "PID  Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, &xPID);
     xTaskCreate(vMenuTask, "MENU Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, &xMenu);
     xTaskCreate(vDataTask, "DATA Task", configMINIMAL_STACK_SIZE * 3, NULL, tskIDLE_PRIORITY + 1, &xData);
@@ -242,21 +245,29 @@ int main(void) {
     vTaskSuspend(xData);
     vTaskSuspend(xPID);
     vTaskSuspend(xExit);
+    
     vTaskResume(xMenu);
+    
+    // Begin the Task Scheduler to Menu Task
     vTaskStartScheduler();
 
     microblaze_disable_interrupts();
-    cleanup_platform(); // This line should be unreachable.
+    cleanup_platform();                 // This line should be unreachable.
 
     return 0;
 }
 
-// Implement PID Control based on data from DataTask()
-// Adjust t_angle based on BTN0 to inc or BTN3 to dec
+/**
+ * @brief The task function for the PID control loop.
+ * 
+ * This task periodically calculates the required correction to minimize the error between the target angle and 
+ * the current angle. The task also handles user input to adjust the target angle, BTNU increases the angle
+ * while BTND decreases it.
+ * 
+ * @param pvParameters Parameters for the task, not used.
+ */
 void vPIDTask(void *pvParameters) {
 
-
-    // TUNED!
     const real_t Kp = 2.0; 
     const real_t Ki = 0.01;
     const real_t Kd = 1.0;
@@ -296,12 +307,17 @@ void vPIDTask(void *pvParameters) {
         updateLEDs(systemState.currentAngle, systemState.targetAngle);
 
         // Display data based on display mode
+        
+        // Check if read mode was asserted
         if((displayMode  == 'r') || (displayMode  == 'R')) {
-            xil_printf("Current Angle = %d, Target Angle = %d, Correction suggestion = %d\r\n", (int)measuredAngle, (int)targetAngle, (int)correction);
+            xil_printf("Current Angle = %d, Target Angle = %d, Correction suggestion = %d\r\n", 
+                    (int)measuredAngle, (int)targetAngle, (int)correction);
         }
 
+        // Check if data gather mode was asserted
         if((displayMode  == 'd') || (displayMode  == 'D')) {
-            xil_printf("%d %d %d\r\n", (int)measuredAngle, (int)targetAngle, (int)correction);
+            xil_printf("%d %d %d\r\n", 
+                    (int)measuredAngle, (int)targetAngle, (int)correction);
         }
         
         // Update target angle before exiting task
@@ -311,29 +327,40 @@ void vPIDTask(void *pvParameters) {
     }
 }
 
-// Display user info
-// Wait for 'r' or 'R' input to resume tasks
+/**
+ * @brief The task function for displaying the user menu and handling user input.
+ * 
+ * This task provides a user interface through UART. It allows the user to activate different modes of operation and 
+ * set the target angle for the system. It also resumes other tasks based on user input.
+ * Use r/R for run mode at the first selection, then select either r/R for read mode or d/D for data mode.
+ * Read mode prints information about the data given, while data mode just prints the raw data, making it easy
+ * to put into a spreadsheet.
+ * 
+ * @param pvParameters Parameters for the task, not used.
+ */
 void vMenuTask(void *pvParameters) {
+   print("DEBUG:Entered vMenuTask()\r\n"); // Log to UART stdout entered Menu Task
 
-    xil_printf("Entered menu\r\n");
+    // Variables to use for retrieving user input from stdin UART
     uint8_t c;
-    char t_angle_str[5];  // largest num is 5 characters {'1', '8', '0', '\r' \0'}
-    for (;;) {
+    char t_angle_str[5];  // largest num is four characters {'1', '8', '0', \0'}
 
+    for (;;) {
         print("INFO:vMenuTask()\tECE 544 Project 2\r\n");
         print("INFO:vMenuTask()\tBy Ibrahim Binmahfood (ibrah5@pdx.edu)\r\n");
         print("INFO:vMenuTask()\tand Robert Wilcox     (rwilcox@pdx.edu)\r\n");
-        print("MENU\r\n");
 
+        // Ask user if to activate run mode
         print("Do wish to activate RUN Mode? Type 'r' or 'R':\r\n");
-        c = XUartLite_RecvByte(UART_BASEADDR);
+        c = XUartLite_RecvByte(UART_BASEADDR);  // retrieve byte character from user
 
+        // Ask user if to activate read mode or gather data mode 
         print("Do you wish to use read mode or data gather mode? Choose 'R/r' or 'D/d':\r\n");
-        displayMode = XUartLite_RecvByte(UART_BASEADDR);
+        displayMode = XUartLite_RecvByte(UART_BASEADDR);    // retrieve byte character from user
 
+        // Check if user activated run mode
         if ((c == 'r') || (c == 'R')) {
-
-            // Prompt for target angle
+            // Ask user for target angle
             print("Set the Target Angle = \r\n");
             
             // Call readAngle to fill t_angle_str with user input
@@ -342,24 +369,37 @@ void vMenuTask(void *pvParameters) {
             // After readAngle, t_angle_str contains the input terminated by '\0'
             systemState.targetAngle = atoi(t_angle_str);    // Convert string to integer
             
-            // Validate and print the target angle
+            // Validate and use the target angle set
+            // Check if target angle within range of 0 <= targetAngle <= 180
             if ((systemState.targetAngle >= 0) && (systemState.targetAngle <= 180)) {
-                xil_printf("User entered %d degrees\r\n", systemState.targetAngle); // Print the target angle
-                xil_printf("Target Angle set to: %d degrees\r\n", systemState.targetAngle); // Print the target angle
+                // Log to UART stdout the target angle value retrieved
+                xil_printf("DEBUG:User entered %d degrees\r\n", systemState.targetAngle);
+                xil_printf("DEBUG:Target Angle set to: %d degrees\r\n", systemState.targetAngle);
+
+                // Resume all other tasks but suspend the Menu Task
                 vTaskResume(xData);
                 vTaskResume(xPID);
                 vTaskResume(xExit);
+
                 vTaskSuspend(xMenu);
-            } else {
+            } 
+
+            // The user entered a value not between 0 and 180 inclusive
+            else {
                 print("ERROR:vMenuTask()\tInvalid Target Angle. Please enter a value between 0 and 180.\r\n");
             }
         }
     }
-
 }
 
-// Collect data from MPU6050 sensor
-// Compute angles and store them in a variable
+/**
+ * @brief The task function for collecting and processing data from the MPU6050 sensor.
+ * 
+ * This task periodically reads data from the gyroscope and accelerometer, processes this data to calculate the current angle,
+ * and updates the system state with this new angle.
+ * 
+ * @param pvParameters Parameters for the task, not used.
+ */
 void vDataTask(void *pvParameters) {
 
     // Set up variable to keep track of cycles for Fusion
@@ -371,79 +411,89 @@ void vDataTask(void *pvParameters) {
     int16_t offsetZ = gyroData.offsetZ;
 
     for (;;) {
-            int16_t gyroX, gyroY, gyroZ;
-            int16_t accelX, accelY, accelZ;
+        int16_t gyroX, gyroY, gyroZ;
+        int16_t accelX, accelY, accelZ;
 
-            // Fetch gyroscope data
-            mpu6050_getGyroData(&i2c_dev, &gyroX, X_AXIS);
-            mpu6050_getGyroData(&i2c_dev, &gyroY, Y_AXIS);
-            mpu6050_getGyroData(&i2c_dev, &gyroZ, Z_AXIS);
+        // Fetch gyroscope data
+        mpu6050_getGyroData(&i2c_dev, &gyroX, X_AXIS);
+        mpu6050_getGyroData(&i2c_dev, &gyroY, Y_AXIS);
+        mpu6050_getGyroData(&i2c_dev, &gyroZ, Z_AXIS);
 
-            // Fetch accelerometer data
-            mpu6050_getAccelData(&i2c_dev, &accelX, &accelY, &accelZ);
+        // Fetch accelerometer data
+        mpu6050_getAccelData(&i2c_dev, &accelX, &accelY, &accelZ);
 
-            // Apply offsets
-            gyroX -= offsetX;
-            gyroY -= offsetY;
-            gyroZ -= offsetZ;
+        // Apply offsets
+        gyroX -= offsetX;
+        gyroY -= offsetY;
+        gyroZ -= offsetZ;
 
-            // Create fusion vector struct with gryo data
-            FusionVector gyroscope = {
-                .axis.x = gyroX * GYRO_TO_RADIANS,
-                .axis.y = gyroY * GYRO_TO_RADIANS,
-                .axis.z = gyroZ * GYRO_TO_RADIANS,
-            };
+        // Create fusion vector struct with gryo data
+        FusionVector gyroscope = {
+            .axis.x = gyroX * GYRO_TO_RADIANS,
+            .axis.y = gyroY * GYRO_TO_RADIANS,
+            .axis.z = gyroZ * GYRO_TO_RADIANS,
+        };
 
-            // Create fusion vector struct with accel data
-            FusionVector accelerometer = {
-                .axis.x = accelX * ACCEL_TO_G,
-                .axis.y = accelY * ACCEL_TO_G,
-                .axis.z = accelZ * ACCEL_TO_G,
-            };
+        // Create fusion vector struct with accel data
+        FusionVector accelerometer = {
+            .axis.x = accelX * ACCEL_TO_G,
+            .axis.y = accelY * ACCEL_TO_G,
+            .axis.z = accelZ * ACCEL_TO_G,
+        };
 
-            // Calculate deltaTime based on ticks
-            TickType_t currentTick = xTaskGetTickCount();
-            float deltaTime = (currentTick - previousTick) / (float)configTICK_RATE_HZ;
-            previousTick = currentTick;
+        // Calculate deltaTime based on ticks
+        TickType_t currentTick = xTaskGetTickCount();
+        float deltaTime = (currentTick - previousTick) / (float)configTICK_RATE_HZ;
+        previousTick = currentTick;
 
-            // Update Fusion AHRS
-            FusionAhrsUpdateNoMagnetometer(&ahrs, gyroscope, accelerometer, deltaTime);
+        // Update Fusion AHRS
+        FusionAhrsUpdateNoMagnetometer(&ahrs, gyroscope, accelerometer, deltaTime);
 
-            // Get and convert orientation to Euler angles
-            FusionQuaternion quaternion = FusionAhrsGetQuaternion(&ahrs);
-            FusionEuler euler = FusionQuaternionToEuler(quaternion);
+        // Get and convert orientation to Euler angles
+        FusionQuaternion quaternion = FusionAhrsGetQuaternion(&ahrs);
+        FusionEuler euler = FusionQuaternionToEuler(quaternion);
 
-            // Update the current angle from the euler struct
-            systemState.currentAngle = euler.angle.roll;
+        // Update the current angle from the euler struct
+        systemState.currentAngle = euler.angle.roll;
 
-            vTaskDelay(pdMS_TO_TICKS(100)); // Task wait
-        }
+        vTaskDelay(pdMS_TO_TICKS(100)); // Task wait
     }
+}
 
-// Check slide switch status
-// Suspend vDataTask() and vPIDTask() if switch is triggered
 void vExitTask(void *pvParameters) {
-    for (;;) {
+  print("DEBUG:Entered vExitTask()\r\n");
 
-        // Get switches
+    for (;;) {
+        // Retrieve current Switches
         uint16_t switches = NX4IO_getSwitches();
-        
-        // Suspend task if any switches are presses
+
+        // Check if any of the switches are asserted
         if (switches & SWITCH_MASK) {
+            // Suspend Data, PID, and Exit tasks except for xMenu
             vTaskSuspend(xData);
             vTaskSuspend(xPID);
             vTaskResume(xMenu);
             vTaskSuspend(xExit);
         }
          
-        vTaskDelay(pdMS_TO_TICKS(500)); // Task wait
+        vTaskDelay(pdMS_TO_TICKS(500)); // Simulate control task workload
     }
-}
+}  
 
-// Initialize system
+/**
+ * @brief The task function for handling system exit conditions.
+ * 
+ * This task monitors for a specific condition (a switch being activated) to suspend data collection and 
+ * PID control tasks, stopping the system's active operations and returning control to the user. This
+ * task implements Fusion Attitude And Heading Reference System package by xioTechnologies to stabilize
+ * gyroscope input and get a verry accurate angle reading.
+ * 
+ * @param pvParameters Parameters for the task, not used.
+ */
 int init_sys(void) {
 
     int status;
+    uint8_t RecvBuffer[1]; 
 
     // Iniitialize NX4IO
     status = NX4IO_initialize(N4IO_BASEADDR);
@@ -471,8 +521,7 @@ int init_sys(void) {
     calibrateGyro(&i2c_dev, &gyroData.offsetX, &gyroData.offsetY, &gyroData.offsetZ);
     xil_printf("Gyro calibrated. Offsets: X=%d, Y=%d, Z=%d\r\n", gyroData.offsetX, gyroData.offsetY, gyroData.offsetZ);
 
-    u8 RecvBuffer[1]; 
-
+    // Check if the WHO_AM_I register in MPU6050 retrieved successfully
     if(readRegister(&i2c_dev, 0x75, RecvBuffer) != XST_SUCCESS) {
         xil_printf("Failed to read WHO_AM_I register\r\n");
     } else {
@@ -483,29 +532,35 @@ int init_sys(void) {
     // Configure MPU6050
     mpu6050_gyroCfg(&i2c_dev, GYRO_FS_SEL_500DPS);
 
-    // Initialize interrupts
+    // Initialize the Interrupt Controller
     status = XIntc_Initialize(&irq_Ctrlr, INTC_DEVICE_ID);
     if (status != XST_SUCCESS) {
         return XST_FAILURE;
     }
 
+    // Start the Interrupt Controller such that interrupts are enabled for all
+    // devices that cause interrupts
     status = XIntc_Start(&irq_Ctrlr, XIN_REAL_MODE);
     if (status != XST_SUCCESS) {
         return XST_FAILURE;
     }
 
-    status = XUartLite_Initialize(&uart_dev, UART_DEVICE_ID);
-    if (status != XST_SUCCESS) {
-        return XST_FAILURE;
-    }
-
+    // Enable the AXI Timer and UART lite interrupts
     XIntc_Enable(&irq_Ctrlr, AXI_TIMER_INTR_NUM);
     XIntc_Enable(&irq_Ctrlr, AXI_UARTLITE_INTR_NUM);
     
     return XST_SUCCESS;
 }
 
-// Reformats buttons to more readble formatt
+/**
+ * @brief Formats button input to a more usable form.
+ * 
+ * This function takes the raw button input values and rearranges them into a format where each bit represents 
+ * a button state in a more readable order. This formatted value is easier to use for button state checks in the program.
+ * 
+ * @param bttns Pointer to the raw button input value.
+ * @return Returns a uint8_t value where each bit represents the state of a button.
+ */
 uint8_t bttn_formatter(uint8_t* bttns) {
     uint8_t bttn_d, bttn_l, bttn_r, bttn_u;
     uint8_t formatted_bttns;
@@ -519,7 +574,16 @@ uint8_t bttn_formatter(uint8_t* bttns) {
     return formatted_bttns;
 }
 
-// Bubble LED function
+/**
+ * @brief Updates the LED display based on the current and target angles, "bubble level"
+ * 
+ * This function calculates which LEDs should be lit based on the current and target angles and updates the 
+ * Nexys4IO board's LEDs accordingly. It ensures that LEDs represent these angles visually for the user.
+ * Two lit LEDs are used to represent the target angle, while a single LED represents the current angle.
+ * 
+ * @param currentAngle The current angle of the system.
+ * @param targetAngle The target angle for the system to achieve.
+ */
 void updateLEDs(real_t currentAngle, real_t targetAngle) {
     uint16_t leds = 0; // Start with all LEDs off
 
@@ -551,7 +615,15 @@ void updateLEDs(real_t currentAngle, real_t targetAngle) {
     NX4IO_setLEDs(leds); // Update the LEDs
 }
 
-// Read the angle input from the user
+/**
+ * @brief Reads an angle value from the UART input.
+ * 
+ * This function captures numeric input from the UART terminal until an end-of-line character is detected. 
+ * It stores this input in a buffer as a string which can then be converted to a numeric value representing an angle.
+ * 
+ * @param buffer The buffer where the input string is stored.
+ * @param bufferSize The size of the buffer.
+ */
 void readAngle(char* buffer, int bufferSize) {
 
     // Count for input
